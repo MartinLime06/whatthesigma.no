d4392b0cd380ee0aa2c704ddf7ea655334c6fbcd
Martin_Lime <57748362+MartinLime06@users.noreply.github.com>
2025-03-14 07:47:11 +0100

    Add Loading component and simplify App component

---
+++ src/Components/Loading.tsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div className="animate-pulse text-7xl">What The Sigma</div>
+    </div>
+  );
+}
+
+export default Loading;
